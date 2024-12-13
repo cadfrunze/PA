ea7335fd7f17a064cbd22cdcc0c5e13efee97244
Marius <marius1890@yahoo.com>
2024-12-13 12:56:15 +0200

    #

--- ticketing-project/Program.cs
+++ ticketing-project/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ;
 
 namespace ticketing_project
 {
@@ -17,6 +18,7 @@ namespace ticketing_project
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            
         }
     }
 }
