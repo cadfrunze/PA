70ab003f95c5b801b83730a2153bdcfd493b90a4
Marius <marius1890@yahoo.com>
2024-12-13 13:36:55 +0200

    #

---
+++ ticketing-project/dbAcess.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace ticketing_project
+{   
+    internal class dbAcess
+    {
+        private const string connectionString =
+            "Data Source=DESKTOP-H2RI6NV\\SQLEXPRESS;Initial Catalog=testing;Integrated Security=True";
+
+        public bool Testconn()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    Console.WriteLine("succes");
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+                return false;
+            }
+        }
+    }
+}
+
+        
+     
