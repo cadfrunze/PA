835c6f81828e2020dc329a86f018c3ae6f9eb551
Marius <marius1890@yahoo.com>
2024-12-13 10:39:57 +0200

    #

---
+++ ticketing-project.data/data_base.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ticketing_project.data
+{
+    internal class data_base
+    {
+    }
+}
