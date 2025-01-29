8538457ff5ed29095092187bd0c5202de32fb6d1
Marius <marius1890@yahoo.com>
2025-01-29 12:03:39 +0200

    Delete testing directory

--- testing/ticketing-project/bonuri/python/data.py
+++
@@ -1,8 +0,0 @@
-import os
-# CONNECTION_STRING: str = os.getenv(r"route_data_base")
-CONNECTION_STRING =(
-    "DRIVER={ODBC Driver 17 for SQL Server};"
-    f"SERVER={os.getenv("device_name")}\\SQLEXPRESS;"
-    f"DATABASE={os.getenv("data_base_name")};"
-    "Trusted_Connection=yes;"
-)
